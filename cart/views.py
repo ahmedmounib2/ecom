b919b1cfb5bb824aeb9385db0e32660cf28d2ce1
ahmedmounib2 <ahmedmounib2@gmail.com>
2024-05-07 17:18:57 +0300

    edited cart persistence after logging out

--- cart/views.py
+++ cart/views.py
@@ -1,87 +1,68 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
 from django.contrib import messages
 
 def cart_summary(request):
-    # Get the cart
-    cart = Cart(request)
-    cart_products = cart.get_prods()
-    quantities = cart.get_quants()
-    totals = cart.cart_total()
-    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities": quantities, "totals":totals})
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	totals = cart.cart_total()
+	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
+
+
 
 
 def cart_add(request):
-    # Get the cart
-    cart = Cart(request)
-    # Test for POST
-    if request.POST.get('action') == 'post':
-        # Get stuff
-        product_id = int(request.POST.get('product_id'))
-        product_qty = int(request.POST.get('product_qty'))
+	# Get the cart
+	cart = Cart(request)
+	# test for POST
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
 
-        # Lookup product in DB
-        product = get_object_or_404(Product, id=product_id)
-        
-        # Save to session
-        cart.add(product=product, quantity=product_qty)
+		# lookup product in DB
+		product = get_object_or_404(Product, id=product_id)
+		
+		# Save to session
+		cart.add(product=product, quantity=product_qty)
 
-        # Get Cart Quantity
-        cart_quantity = len(cart)
+		# Get Cart Quantity
+		cart_quantity = cart.__len__()
 
-        # Return response
-        response = JsonResponse({'qty': cart_quantity})
-        messages.success(request, "Product Added To Cart...")
-        return response
+		# Return resonse
+		# response = JsonResponse({'Product Name: ': product.name})
+		response = JsonResponse({'qty': cart_quantity})
+		messages.success(request, ("Product Added To Cart..."))
+		return response
+
+def cart_delete(request):
+	cart = Cart(request)
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		# Call delete Function in Cart
+		cart.delete(product=product_id)
+
+		response = JsonResponse({'product':product_id})
+		#return redirect('cart_summary')
+		messages.success(request, ("Item Deleted From Shopping Cart..."))
+		return response
 
 
 def cart_update(request):
-    if request.method == 'POST' and 'product_qty' in request.POST:
-        try:
-            # Get the product ID and quantity from the POST request
-            product_id = int(request.POST.get('product_id'))
-            product_qty_str = request.POST.get('product_qty')
+	cart = Cart(request)
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
 
-            # Check if the quantity string is not empty
-            if product_qty_str.strip():
-                product_qty = int(product_qty_str)
-            else:
-                # If quantity is empty, raise a ValueError
-                raise ValueError("Product quantity is empty")
+		cart.update(product=product_id, quantity=product_qty)
 
-            # Update the cart with the new quantity
-            cart = Cart(request)  # Instantiate the Cart class with the request object
-            cart.update(product=product_id, quantity=product_qty)
-
-            # Prepare JSON response
-            response = JsonResponse({'qty': product_qty})
-
-            # Optionally, you can redirect to another page or display a success message
-            # return redirect('cart_summary')
-            messages.success(request, "Your Cart Has Been Updated...")
-
-            return response
-        except ValueError as e:
-            # Handle the ValueError (e.g., log the error, return an error response)
-            return JsonResponse({'error': str(e)}, status=400)
-
-
-def cart_delete(request):
-    cart = Cart(request)
-    if request.method == 'POST' and 'product_id' in request.POST:
-        # Get product ID from POST data
-        product_id = int(request.POST.get('product_id'))
-        # Call delete function in cart
-        cart.delete(product=product_id)
-        # Prepare JSON response
-        response = JsonResponse({'success': 'Product removed from cart successfully'})
-        messages.success(request, "Item Deleted From Shopping Cart...")
-
-        return response
-    else:
-        # If product ID is not found in POST data, return an error response
-        response = JsonResponse({'error': 'Product ID not provided or invalid'})
-        response.status_code = 400
-        return response
+		response = JsonResponse({'qty':product_qty})
+		#return redirect('cart_summary')
+		messages.success(request, ("Your Cart Has Been Updated..."))
+		return response
--- store/urls.py
+++ store/urls.py
@@ -2,21 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('login/', views.login_user, name='login'),
-  path('logout/', views.logout_user, name='logout'),
-  path('all-products/', views.all_products, name='all_products'),
-  path('popular/', views.popular_items, name='popular_items'),
-  path('new-arrivals/', views.new_arrivals, name='new_arrivals'),
-  path('register/', views.register_user, name='register'),
-  path('update_password/', views.update_password, name='update_password'),
-  path('update_user/', views.update_user, name='update_user'),
-  path('update_info/', views.update_info, name='update_info'),
-  path('product/ <int:pk>', views.product, name='product'),
-  path('category/ <str:foo>', views.category, name='category'),
-  path('category_summary/', views.category_summary, name='category_summary'),
-  path('search/', views.search, name='search'),
-
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_info/', views.update_info, name='update_info'),
+    path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'),
+    path('category_summary/', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
+    path('popular_items/', views.popular_items, name='popular_items'),
+    path('new_arrivals/', views.new_arrivals, name='new_arrivals'),
 
 ]
